38ca554a780a8200a983eb5a19ea8599820deb12
Francisco Albusac <francis.jaa@protonmail.com>
2021-11-25 16:45:28 +0100

    more

--- learn-dbt/models/example/snowflake_customer_purchases.sql
+++ learn-dbt/models/example/snowflake_customer_purchases.sql
@@ -6,7 +6,5 @@ select
     c.c_nationkey as nation,
     sum(o.o_totalprice) as total_order_price
 from {{ source('sample', 'customer') }} c
-left join "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
-on c.c_custkey = o.o_custkey
-{{group_by(3)}} -- same than group by 1,2,3, but using custom macro
-having sum(o.o_totalprice) > 0
+left join "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o on c.c_custkey = o.o_custkey
+{{group_by(3)}} having sum(o.o_totalprice) > 0 -- same than group by 1,2,3, but using custom macro
