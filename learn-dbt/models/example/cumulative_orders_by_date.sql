082ab4d28278afb47af34a6fd708a4a7f2b2087c
Francisco Albusac <francis.jaa@protonmail.com>
2021-11-25 16:45:01 +0100

    more

--- learn-dbt/models/example/cumulative_orders_by_date.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized="view") }}
-
-select 
-    distinct o_orderdate,
-    sum(o_totalprice) over (order by o_orderdate) as cumulative_sales
-from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
-order by o_orderdate
